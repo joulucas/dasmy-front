fd591d6c1bfc08770b85c5dd09006af21a701c26
joulucas <joubertvh1@hotmail.com>
2022-06-17 15:53:58 -0300

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './scr/app/pages/home/home.component';
+import {TabMenuModule} from "primeng/tabmenu";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        TabMenuModule,
+        RouterModule.forRoot([
+        {path:'',component: AppComponent},
+      ])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/scr/app/pages/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from "primeng/api";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  items: MenuItem[];
+
+  constructor() {
+    this.items = [
+      {label: 'Cadastro', icon: 'pi pi-fw pi-users'},
+      {label: 'Vendas', icon: 'pi pi-fw pi-money-bill'},
+      {label: 'Relatorios', icon: 'pi pi-fw pi-paperclip'},
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
